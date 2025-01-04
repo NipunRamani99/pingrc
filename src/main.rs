2b654a883d53b07a1d8df3c4a5ee1d669510553b
Nipun Ramani <ramaninipun@gmail.com>
2025-01-04 22:52:30 +0000

    minor refactoring

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,31 @@ use std::net::{IpAddr, Ipv4Addr};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
-    if env::args().len() < 2 {
+    if env::args().len() < 2 
+    {
         println!("Usage: pingrc target_ip [num_pings]");
         return;
     }
     
     let target_ip: IpAddr = env::args().nth(1).unwrap().parse().expect("Couldn't parse argument for IPv4 address.");
     let mut num_pings = 4u16;
+    
     if env::args().len() == 3 
     {
         num_pings = env::args().nth(2).unwrap().parse().expect("Couldn't parse number of pings.")
     }
+    
     let protocol_icmp = Layer4(Ipv4(IpNextHeaderProtocols::Icmp));
     let (mut transmitter, mut receiver) = transport_channel(1024, protocol_icmp).unwrap();
-
+    
     let mut max_time = 0;
     let mut min_time = u32::MAX;
     let mut accumulated_time = 0u32;
     let mut num_succeeded = 0u16;
     println!("Pinging {} with 32 bytes of data", target_ip);
-    for i in 0..num_pings {
-        let payload: Vec<u8> = vec![0u8];
+
+    for i in 0..num_pings 
+    {
         let mut buffer = [0u8; 64]; // Adjust the size as needed
         let mut icmp_packet = MutableIcmpPacket::new(&mut buffer).unwrap();
         let mut payload: Vec<u8> = vec![0u8; 32];
@@ -50,11 +54,15 @@ fn main() {
         let since_the_epoch = start
             .duration_since(UNIX_EPOCH)
             .expect("Time went backwards");
-        match transmitter.send_to(icmp_packet, target_ip) {
-            Ok(s) => {
+        match transmitter.send_to(icmp_packet, target_ip) 
+        {
+            Ok(s) => 
+            {
                 let mut iter = icmp_packet_iter(&mut receiver);
-                match iter.next() {
-                    Ok((packet, addr)) => {
+                match iter.next() 
+                {
+                    Ok((packet, addr)) => 
+                    {
                         let end = SystemTime::now();
                         let resp_payload = packet.payload();
                         if packet.get_checksum() != pnet::packet::icmp::checksum(&packet.to_immutable())
@@ -85,7 +93,9 @@ fn main() {
             Err(e) => println!("Failed to send ICMP Packet, error: {}", e),
         }
     }
+
     let average = (accumulated_time as f32)/(num_pings as f32);
+    
     println!("{} out of {} packets received successfully.", num_succeeded, num_pings);
     println!("Approximate round trip times in milli-seconds: ");
     println!("Max: {}ms, Min: {}ms, Average: {}ms", max_time, min_time, average);
