4e293f057befcd8ffdff7432b4dabb50afb6fed1
Nipun Ramani <ramaninipun@gmail.com>
2025-01-04 22:49:32 +0000

    Added readme and checks to validate response packets

---
+++ README.md
@@ -0,0 +1,34 @@
+Implemented ```ping``` in Rust using ```pnet```.
+### Build
+Nothing special for Linux/MacOS, just run ```cargo build```.  
+
+Before running ```cargo build```, download Packet.lib from [WinPcap Developer Pack](https://www.winpcap.org/devel.htm) and place it in your repository like this:
+```
+pingrc
+ ├── Cargo.lock
+ ├── Cargo.toml
+ ├── Packet.lib
+ ├── README.md
+ ├── src
+ └── target
+ ``` 
+
+More information on this requirement can be found on the github repository for [libpnet](https://github.com/libpnet/libpnet/tree/main).
+
+
+### Usage
+```
+./pingrc target_ip [num_pings]
+```
+Sample output
+```
+PS D:\Rust\pingrc\target\debug> ./pingrc.exe 103.243.32.90 4
+Pinging 103.243.32.90 with 32 bytes of data
+Reply from: 103.243.32.90 in 162ms
+Reply from: 103.243.32.90 in 161ms
+Reply from: 103.243.32.90 in 161ms
+Reply from: 103.243.32.90 in 162ms
+4 out of 4 packets received successfully.
+Approximate round trip times in milli-seconds:
+Max: 162ms, Min: 161ms, Average: 161.5ms
+```
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use rand::Rng;
 use std::cmp;
 use pnet::packet::icmp::{IcmpCode, IcmpTypes, MutableIcmpPacket};
 use pnet::packet::ip::IpNextHeaderProtocols;
-use pnet::packet::{self, Packet};
+use pnet::packet::{self, MutablePacket, Packet};
 use pnet::transport::TransportChannelType::Layer4;
 use pnet::transport::TransportProtocol::Ipv4;
 use pnet::transport::{icmp_packet_iter, transport_channel};
@@ -16,18 +16,23 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
     if env::args().len() < 2 {
-        println!("Usage: ping target_address");
+        println!("Usage: pingrc target_ip [num_pings]");
         return;
     }
-    let target_ip: IpAddr = env::args().nth(1).unwrap().parse().expect("Parse failed");
-
+    
+    let target_ip: IpAddr = env::args().nth(1).unwrap().parse().expect("Couldn't parse argument for IPv4 address.");
+    let mut num_pings = 4u16;
+    if env::args().len() == 3 
+    {
+        num_pings = env::args().nth(2).unwrap().parse().expect("Couldn't parse number of pings.")
+    }
     let protocol_icmp = Layer4(Ipv4(IpNextHeaderProtocols::Icmp));
     let (mut transmitter, mut receiver) = transport_channel(1024, protocol_icmp).unwrap();
 
     let mut max_time = 0;
     let mut min_time = u32::MAX;
-    let mut average = 0u32;
-    let mut num_pings = 20;
+    let mut accumulated_time = 0u32;
+    let mut num_succeeded = 0u16;
     println!("Pinging {} with 32 bytes of data", target_ip);
     for i in 0..num_pings {
         let payload: Vec<u8> = vec![0u8];
@@ -51,12 +56,28 @@ fn main() {
                 match iter.next() {
                     Ok((packet, addr)) => {
                         let end = SystemTime::now();
+                        let resp_payload = packet.payload();
+                        if packet.get_checksum() != pnet::packet::icmp::checksum(&packet.to_immutable())
+                        {
+                            println!("Checksum Discrepency");
+                        }
+                        else 
+                        {
+                            num_succeeded += 1;
+                        }
+                        if payload != resp_payload[0..32]
+                        {
+                            println!("Response payload discrepency");
+                            println!("Request Payload: {:?}", payload);
+                            println!("Response Payload: {:?}", &resp_payload[0..32]);
+                            
+                        }
                         let since_start = end.duration_since(start).expect("Time went backwards");
                         let since_start_in_ms = since_start.as_millis();
                         println!("Reply from: {} in {}ms", addr, since_start_in_ms);
                         max_time = cmp::max(max_time, since_start_in_ms as u32);
                         min_time = cmp::min(min_time, since_start_in_ms as u32);
-                        average += since_start_in_ms as u32;
+                        accumulated_time += since_start_in_ms as u32;
                     }
                     Err(e) => panic!("ICMP packet receive error: {}", e),
                 }
@@ -64,7 +85,9 @@ fn main() {
             Err(e) => println!("Failed to send ICMP Packet, error: {}", e),
         }
     }
-    average = average/num_pings;
+    let average = (accumulated_time as f32)/(num_pings as f32);
+    println!("{} out of {} packets received successfully.", num_succeeded, num_pings);
     println!("Approximate round trip times in milli-seconds: ");
     println!("Max: {}ms, Min: {}ms, Average: {}ms", max_time, min_time, average);
+    
 }
